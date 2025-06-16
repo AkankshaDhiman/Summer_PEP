a3ef4e9731b534c8be38285b74fe673d74feb322
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-06-16 11:04:09 +0530

    Initial commit

---
+++ src/day1_code1.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class day1_code1 {
+    int a=10;
+    String s="hello";
+
+    public static void main(String[] args) throws IOException {
+        day1_code1 obj= new day1_code1();
+        int x=10;
+        obj.m();
+    }
+
+    void m() {
+        System.out.println("16june");
+    }
+}
---
+++ src/day1_code2.java
@@ -0,0 +1,21 @@
+public class day1_code2 {
+//    void a(){
+//        System.out.println("day a");
+//        //c();
+//    }
+//    void b(){
+//        System.out.println("day b");
+//        a();
+//    }
+//    void c(){
+//        System.out.println("day c");
+//        b();
+//    }
+    public static void main(String[] args) {
+        day1_code2 d1 = new day1_code2();
+//        d1.c();
+        String s= new String("Hello");
+        s=s.concat("World");
+        System.out.println(s);
+    }
+}
