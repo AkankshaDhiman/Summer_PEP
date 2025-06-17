510c9278ee551c3ccf4621a34db2aaca6a995361
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-06-17 09:28:06 +0530

    avg length of words

--- src/day2_code1_avg_length_of_words.java
+++ src/day2_code1_avg_length_of_words.java
@@ -5,14 +5,20 @@ public class day2_code1_avg_length_of_words {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        String s = sc.nextLine();
-        StringTokenizer st = new StringTokenizer(s);
-        int totalLen = 0, count = 0;
-        while (st.hasMoreTokens()) {
-            String word = st.nextToken();
+       String[] words = s.split(" ");
+       int totalLen = 0;
+       int Count = words.length;
+        for (String word : words) {
             totalLen += word.length();
-            count++;
         }
-        double avg = (double)totalLen / count;
+//        StringTokenizer st = new StringTokenizer(s);
+//        int totalLength = 0, count = 0;
+//        while (st.hasMoreTokens()) {
+//            String word = st.nextToken();
+//            totalLength += word.length();
+//            count++;
+//        }
+        double avg = (double)totalLen / Count;
         System.out.println(avg);
     }
 }
