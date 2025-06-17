3d7fff12af2b1a4609a2d1425cbb6fab3849b1c3
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-06-17 14:07:49 +0530

    Automatic promotion and method overloading

--- src/Automatic_promotion_and_method_overloading.java
+++ src/Automatic_promotion_and_method_overloading.java
@@ -20,28 +20,27 @@ public class Automatic_promotion_and_method_overloading {
 //        System.out.println("successfully sb invoked");
 //        }  //getting ambigous unexceptional call due to string and stringbuffer(compiler gets confused)
 
-        void m(int ... i){
-            System.out.println("i... invoked");
-        }
-
+//
         void m(int a, int b, int c){
         System.out.println(" a,b,c successful");
         }
+
         void m(int a, float b){
         System.out.println("float args method");
         }
+
     public static void main(String[] args) {
         Automatic_promotion_and_method_overloading obj = new Automatic_promotion_and_method_overloading();
-//        obj.m();
-//        obj.m( 10);
-//        obj.m('c');
-//
-//        String s= new String("Hello");
-//        Object o= new Object();
-//        obj.m(s);
-//        obj.m(o);
-//        obj.m(1.00);// error because double/float consumes more memory
-//        obj.m(null); //for strings and object
+        obj.m();
+        obj.m( 10);
+        obj.m('c');
+
+        String s= new String("Hello");
+        Object o= new Object();
+        obj.m(s);
+        obj.m(o);
+        obj.m(1.00);// error because double/float consumes more memory
+        obj.m(null); //for strings and object
         //child class gets preference over parent class during function call in method overloading
 
         obj.m(10,15,7);
