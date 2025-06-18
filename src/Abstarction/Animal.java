2372c217c83735f5695c904d420a54ee415e4aae
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-06-18 09:34:38 +0530

    Abstraction/18/June

---
+++ src/Abstarction/Animal.java
@@ -0,0 +1,32 @@
+package Abstarction;
+
+public abstract class Animal {
+
+    abstract void sound();
+
+   abstract void movement();
+
+   void eat(){
+       System.out.println("Animal is Eating...");
+   }
+}
+abstract class Dog extends Animal {
+    void sound(){
+        System.out.println("Dog is barking.");
+    }
+
+}
+
+class Puppy extends Dog {
+    void movement() {
+        System.out.println("Puppy is walking.");
+    }
+}
+class Main{
+    public static void main(String[] args) {
+    Dog dog = new Puppy();
+    dog.movement();
+    dog.eat();
+    dog.sound();
+    }
+}
